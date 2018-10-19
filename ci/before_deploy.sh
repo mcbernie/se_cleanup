243689c646715a5932cbac414f6c1ac2f63dcd84
Nicolas Brüggemann <nbrueggemann@itcoops.de>
2018-10-19 15:32:51 +0200

    change travis settings #10

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -2,6 +2,11 @@
 
 set -ex
 
+export OPENSSL_STATIC=0
+export OPENSSL_DIR=/tmp/openssl-1.1.1-win64-mingw
+export OPENSSL_LIB_DIR=/tmp/openssl-1.1.1-win64-mingw/lib
+export OPENSSL_INCLUDE_DIR=/tmp/openssl-1.1.1-win64-mingw/include
+
 main() {
     local src=$(pwd) \
           stage=
@@ -17,14 +22,14 @@ main() {
 
     test -f Cargo.lock || cargo generate-lockfile
 
-
     export OPENSSL_STATIC=0
     export OPENSSL_DIR=/tmp/openssl-1.1.1-win64-mingw
     export OPENSSL_LIB_DIR=/tmp/openssl-1.1.1-win64-mingw/lib
     export OPENSSL_INCLUDE_DIR=/tmp/openssl-1.1.1-win64-mingw/include
 
+    ls -All /tmp/openssl-1.1.1-win64-mingw
     # TODO Update this to build the artifacts that matter to you
-    cross rustc --target $TARGET --release -- -C lto
+    env OPENSSL_STATIC=0 OPENSSL_DIR=/tmp/openssl-1.1.1-win64-mingw cross rustc --target $TARGET --release -- -C lto
 
     # TODO Update this to package the right artifacts
     cp target/$TARGET/release/se_shell $stage/
