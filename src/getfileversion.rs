7c5cf205f34b1376daeb1d9344fbbfa0445507ca
Nicolas Brüggemann <nbrueggemann@itcoops.de>
2018-10-23 08:43:42 +0200

    create simple test

--- src/getfileversion.rs
+++ src/getfileversion.rs
@@ -6,8 +6,10 @@
 use std::path::PathBuf;
 
 type DWORD = u32;
+#[allow(dead_code)]
 type WORD = u16;
 
+#[allow(dead_code)]
 #[allow(unused_variables)]
 #[allow(non_snake_case)]
 #[repr(C)]
@@ -27,11 +29,15 @@ struct VS_FIXEDFILEINFO {
     dwFileDateLS: DWORD,
 }
 
-
+#[allow(dead_code)]
+#[allow(non_snake_case)]
 #[inline]
 fn LOWORD(l: DWORD) -> WORD {
     (l & 0xffff) as WORD
 }
+
+#[allow(dead_code)]
+#[allow(non_snake_case)]
 #[inline]
 fn HIWORD(l: DWORD) -> WORD {
     ((l >> 16) & 0xffff) as WORD
@@ -39,6 +45,7 @@ fn HIWORD(l: DWORD) -> WORD {
 
 
 #[cfg(not(windows))]
+#[allow(unused_variables)]
 pub fn get_file_version(file: PathBuf) -> (u16,u16,u16,u16) {
      (0,0,0,0)
 }
@@ -53,11 +60,11 @@ pub fn get_file_version(file: PathBuf) -> (u16,u16,u16,u16) {
     use std::ptr::null_mut;
     use std::mem;
 
-    use winapi::*;
-    use winapi::um::winver::{GetFileVersionInfoW, GetFileVersionInfoSizeW, VerQueryValueW};
+    use self::winapi::*;
+    use self::winapi::um::winver::{GetFileVersionInfoW, GetFileVersionInfoSizeW, VerQueryValueW};
 
 
-    let wide: Vec<_> = OsStr::new(file.display()).encode_wide().chain(once(0)).collect();
+    let wide: Vec<_> = OsStr::new(&file.display()).encode_wide().chain(once(0)).collect();
 
     let ret_size = unsafe {
         GetFileVersionInfoSizeW(wide.as_ptr(), null_mut())
@@ -115,4 +122,32 @@ pub fn get_file_version(file: PathBuf) -> (u16,u16,u16,u16) {
         HIWORD(info.dwProductVersionLS),
         LOWORD(info.dwProductVersionLS),
     )
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    /*#[cfg(windows)]
+    #[test]
+    fn test_file_version_in_windows() {
+        let r = get_file_version(PathBuf::from("abcdefg"));
+        assert_eq!(r, (0,0,0,0));
+    }*/
+
+    /*#[cfg(not(windows))]
+    #[test]
+    fn test_file_version_not_in_windows() {
+        let r = get_file_version(PathBuf::from("abcdefg"));
+        assert_eq!(r, (0,0,0,0));
+    }*/
+
+    #[test]
+    fn test_same_result_on_all_systems() {
+        let r = get_file_version(PathBuf::from("abcdefg"));
+        assert_eq!(r, (0,0,0,0));
+    }
+
 }
