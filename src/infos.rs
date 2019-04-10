df9ff8e7882006ad4fc20ffe8d74ec7acdad675b
Nicolas Brüggemann (mcbernie) <nb@zweiund40.com>
2019-04-10 15:36:23 +0200

    add user settings to wmic command f

--- src/infos.rs
+++ src/infos.rs
@@ -4,12 +4,13 @@
 pub fn password_expires() -> Result<bool, String> {
     use std::process::Command;
     let output = Command::new("wmic")
-        .args(&["/user:'se_user'","/password:'starentertainer'","useraccount where Name='se_user' GET PasswordExpires"])
+        .args(&["useraccount where Name=\"se_user\" GET PasswordExpires"])
         .output()
         .expect("failed to execute wmic useraccount");
 
     let out = String::from_utf8_lossy(&output.stdout);
     let out_err = String::from_utf8_lossy(&output.stderr);
+
 
     if out.contains("FALSE") {
         Ok(false)
@@ -25,18 +26,32 @@ pub fn password_expires() -> Result<bool, String> {
 pub fn set_password_expires_to_false() -> Result<(), &'static str> {
     use std::process::Command;
     let output = Command::new("wmic")
-        .args(&["/user:'se_user'","/password:'starentertainer'","useraccount where Name='se_user' SET PasswordExpires=false"])
+        .args(&["useraccount where Name=\"se_user\" SET PasswordExpires=false"])
         .output()
         .expect("failed to execute wmic useraccount to set passwordexpires");
 
     // get second line of output
     let _out = String::from_utf8_lossy(&output.stdout);
-    /*let lines: Vec<&str> = out.split("\r\n").collect();
-    if lines.len() < 2 {
-        return Err("Not enough lines");
-    }*/
 
-    //let value = lines[1];
+/*
+    let output_pwage = Command::new("net")
+        .args(&["accounts","/MaxPWAge:unlimited"])
+        .output()
+        .expect("failed to execute wmic useraccount");
+
+    let _out_pwage = String::from_utf8_lossy(&output_pwage.stdout);
+    //let out_pwage_err = String::from_utf8_lossy(&output_pwage.stderr);
+
+
+    let output_epxires = Command::new("net")
+        .args(&["user","se_user/expires:never"])
+        .output()
+        .expect("failed to execute wmic useraccount");
+
+    let _out_epxires = String::from_utf8_lossy(&output_epxires.stdout);
+    //let out_epxires_err = String::from_utf8_lossy(&output_epxires.stderr);
+
+*/
 
     Ok(())
 }
@@ -44,7 +59,7 @@ pub fn set_password_expires_to_false() -> Result<(), &'static str> {
 pub fn reboot_system() -> Result<(), &'static str> {
     use std::process::Command;
     let output = Command::new("wmic")
-        .args(&["/user:'se_user'","/password:'starentertainer'","os where Primary='TRUE' reboot"])
+        .args(&["os where Primary='TRUE' reboot"])
         .output()
         .expect("failed to execute Primary");
 
